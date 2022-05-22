de8403bb1137ae5e61377b8e455b9cbb1f605176
Yanis Dib <ynsprive@gmail.com>
2022-05-22 15:12:30 +0200

    Create an index for interfaces import

---
+++ src/interfaces/index.ts
@@ -0,0 +1,4 @@
+export type { Article } from './Article';
+export type { Comment } from './Comment';
+export type { ImageAttributes } from './ImageAttributes';
+export type { Topic } from './Topic';
