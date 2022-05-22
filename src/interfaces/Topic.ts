39d102b13be28f4bc3de5646eeaaf45d76ca43ee
Yanis Dib <ynsprive@gmail.com>
2022-05-22 17:23:46 +0200

    Update Topic interface

--- src/interfaces/Topic.ts
+++ src/interfaces/Topic.ts
@@ -3,8 +3,8 @@
  * an article's topic should be created with
  */
 export interface Topic {
+    createdAt: number;
     id: string;
     name: string;
     permalink: string;
-    createdAt: number;
 }
