21c91d981efd414debf4ddd179706a4f048a3979
Yanis Dib <ynsprive@gmail.com>
2022-05-22 19:40:48 +0200

    Update interfaces imports

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -2,3 +2,6 @@ export type { Article } from './Article';
 export type { Comment } from './Comment';
 export type { ImageAttributes } from './ImageAttributes';
 export type { Topic } from './Topic';
+
+export type { FailedAction } from './FailedAction';
+export type { StoreState } from './StoreState';
