62c05c7aff51cd3e4aaa352d105fdca16b82ff67
Yanis Dib <ynsprive@gmail.com>
2022-05-22 17:24:05 +0200

    Create API requests for articles

---
+++ src/services/api/articles.ts
@@ -0,0 +1,37 @@
+import axios, { AxiosResponse } from 'axios';
+
+import { Article, HttpRequestOptions } from '../../interfaces';
+
+
+const ROOT_API_URL = process.env.REACT_APP_API_URL;
+
+export const fetchArticles = async (): Promise<Article[]> => {
+    const url: string = `${ROOT_API_URL}/articles`;
+
+    try {
+        const response: AxiosResponse<Article[]> = await axios.get(url);
+
+        return response.data;
+    } catch (error) {
+        throw error;
+    }
+}
+
+/**
+ * Fetch Articles and retrieve the article
+ * matching the permalink passed in
+ */
+export const fetchArticleByPermalink = async (
+    permalink: string
+): Promise<Article | object> => {
+    try {
+        const results: Article[] = await fetchArticles();
+        const article: Article | undefined = results.find(
+            (article: Article) => article.permalink === permalink
+        );
+
+        return article ?? {};
+    } catch (error) {
+        throw error;
+    }
+}
