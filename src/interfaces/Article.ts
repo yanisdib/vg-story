12a1bc3d28e657ab2208f548ee9fd4e5952a6664
Yanis Dib <ynsprive@gmail.com>
2022-05-22 15:11:37 +0200

    Create an Article interface

---
+++ src/interfaces/Article.ts
@@ -0,0 +1,28 @@
+import {
+    Comment,
+    ImageAttributes,
+    Topic
+} from '.';
+
+
+/**
+ * Article is an interface which describes properties
+ * an article should be created with
+ */
+export interface Article {
+    author: string;
+    backgroundHex?: string;
+    body?: string;
+    comments?: Comment[];
+    createdAt: number;
+    editedAt: number;
+    frontImage: ImageAttributes;
+    imageGallery?: {
+        id: string,
+        images: ImageAttributes[]
+    }[];
+    id: string;
+    isFeatured?: boolean;
+    title: string;
+    topics: Topic[];
+}
