8306e51ac307af40a2a323a7d98dea32fd9c8840
Yanis Dib <ynsprive@gmail.com>
2022-05-23 19:17:42 +0200

    Add App Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
+
+import { Footer } from './components';
+import { Home } from './pages';
+
+
 function App(): JSX.Element {
   return (
-    <div>
-
-    </div>
+    <main>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <Footer />
+    </main>
   );
 }
 
