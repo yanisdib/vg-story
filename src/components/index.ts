fa3d71cd05fe31262873545eccc12c4feb64f5f1
Yanis Dib <ynsprive@gmail.com>
2022-05-23 19:18:06 +0200

    Add new imports

---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+export { default as Container } from './Container/Container';
+export { default as Footer } from './Footer/Footer';
+
+export { default as Spinner } from './Spinner/Spinner';
+export { default as Loading } from './Loading/Loading';
