d9088fee22d4f4ad163e072b29fb763c6b1c073a
Yanis Dib <ynsprive@gmail.com>
2022-05-23 19:24:16 +0200

    Update Spinner component

--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -14,8 +14,8 @@ const spin = keyframes`
 export default styled.div`
     width: 2.28571429rem;
     height: 2.28571429rem;
-    border: 0.2em solid #d1d1d1;
-    border-top: 0.2em solid #ff8bb7;
+    border: 0.2em solid #D1D1D1;
+    border-top: 0.2em solid #FF8BB7;
     border-radius: 50%;
     animation: ${spin} 0.6s linear infinite;
 `;
