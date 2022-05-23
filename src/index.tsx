4080d7f5bac782c2f55e93d6502edd10154ef548
Yanis Dib <ynsprive@gmail.com>
2022-05-23 19:21:20 +0200

    Add ThemeProvider, BrowserRouter and StoreProvider

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import { Provider as StoreProvider } from 'react-redux';
+
+import store from './services/redux/store';
+
+import GlobalTheme from './assets/styles/global';
+import { defaultTheme } from './assets/styles/theme';
 
 import App from './App';
 
@@ -10,7 +18,14 @@ const root: ReactDOM.Root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalTheme />
+      <StoreProvider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </StoreProvider>
+    </ThemeProvider>
   </React.StrictMode>
 );
 
