d1be8971a48f3f39f1a2e9f11705aab55405eef1
Yanis Dib <ynsprive@gmail.com>
2022-05-24 22:15:47 +0200

    Add Hero component import

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,5 @@ export { default as Footer } from './Footer/Footer';
 
 export { default as Spinner } from './Spinner/Spinner';
 export { default as Loading } from './Loading/Loading';
+
+export { default as Hero } from './Hero/Hero';
