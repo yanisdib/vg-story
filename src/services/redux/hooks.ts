6ccb81cc99991c5d268efd172268a34d48262646
Yanis Dib <ynsprive@gmail.com>
2022-05-24 22:16:52 +0200

    Create Redux hooks

---
+++ src/services/redux/hooks.ts
@@ -0,0 +1,6 @@
+import { useSelector, TypedUseSelectorHook } from 'react-redux';
+
+import { RootState } from './store';
+
+
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
