29a27447d0208d1be3e7da23eef3b3a1afbab87d
Yanis Dib <ynsprive@gmail.com>
2022-05-24 22:16:39 +0200

    Implement Hero component

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,31 @@
+import { Hero } from '../../components';
+
+import { Article } from '../../interfaces';
+
+import { useArticles } from '../../hooks/useArticles';
+import { selectFeaturedArticles } from '../../services/redux/selectors/articles';
+
+import withStatusHandler from '../../helpers/hoc/withStatusHandler';
+
+
 function Home(): JSX.Element {
-    return <div>Homepage</div>;
+    const { articles, error, status } = useArticles();
+
+    const featuredArticles = selectFeaturedArticles({
+        body: articles,
+        error,
+        status
+    });
+
+    const HeroWithStatusHandler = withStatusHandler<{ article: Article }>(Hero);
+
+    return (
+        <HeroWithStatusHandler
+            error={error}
+            status={status}
+            {...{ article: featuredArticles[0] }}
+        />
+    );
 }
 
 
