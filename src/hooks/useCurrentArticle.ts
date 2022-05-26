0856fbfa5f820a00110856784895e9939151a0e5
Yanis Dib <ynsprive@gmail.com>
2022-05-27 00:00:33 +0200

    Create hook for currentArticle

---
+++ src/hooks/useCurrentArticle.ts
@@ -0,0 +1,25 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { useParams } from 'react-router-dom';
+
+import { useAppSelector } from '../services/redux/hooks';
+import { fetchCurrentArticleRequest } from '../services/redux/currentArticle/currentArticleSlice';
+
+
+export function useCurrentArticle() {
+    const dispatch = useDispatch();
+
+    const { permalink = '' } = useParams();
+
+    useEffect(() => {
+        dispatch(fetchCurrentArticleRequest(permalink));
+    }, [permalink]);
+
+    const {
+        body,
+        error,
+        status
+    } = useAppSelector(state => state.currentArticle);
+
+    return { currentArticle: body, error, status };
+}
