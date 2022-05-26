369396399fb57f692d2fc626a1595bf56a426cd5
Yanis Dib <ynsprive@gmail.com>
2022-05-27 00:01:37 +0200

    Add currentArticle saga

--- src/services/redux/rootSaga.ts
+++ src/services/redux/rootSaga.ts
@@ -1,10 +1,12 @@
 import { all, fork } from '@redux-saga/core/effects';
 
 import articlesSaga from './sagas/articles';
+import currentArticleSaga from './sagas/currentArticle';
 
 
 export default function* rootSaga() {
     yield all([
-        fork(articlesSaga)
+        fork(articlesSaga),
+        fork(currentArticleSaga)
     ]);
 }
