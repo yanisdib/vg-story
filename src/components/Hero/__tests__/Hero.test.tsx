585d82422c744f3d5948d49993b1069ca72222c4
Yanis Dib <ynsprive@gmail.com>
2022-05-26 18:35:47 +0200

    Create tests for Hero component

--- src/components/Hero/__tests__/Hero.test.tsx
+++ src/components/Hero/__tests__/Hero.test.tsx
@@ -10,7 +10,7 @@ describe('Hero component', () => {
 
     const article: Article = {
         author: 'Yanis',
-        backgroundHex: '#000',
+        backgroundHex: '#FF5B5B',
         frontImage: { src: 'hello.jpg' },
         createdAt: 1000,
         id: '12de',
@@ -44,5 +44,42 @@ describe('Hero component', () => {
         );
 
         expect(getByText('This is a test')).toBeInTheDocument();
+    });
+
+    it('should have background color of the article', () => {
+
+        const { getByTestId } = render(
+            <Hero article={article} />,
+            { wrapper: MemoryRouter }
+        );
+
+        expect(getByTestId('hero-section'))
+            .toHaveStyle(`background-color: ${article.backgroundHex}`);
+    });
+
+    it('should have a black background by default', () => {
+        const testArticle: Article = {
+            author: 'Yanis',
+            frontImage: { src: 'hello.jpg' },
+            createdAt: 1000,
+            id: '12de',
+            permalink: 'this-is-a-test',
+            title: 'This is a test',
+            topics: [
+                {
+                    createdAt: 0,
+                    id: '1ac',
+                    name: 'Test',
+                    permalink: 'test'
+                }
+            ]
+        };
+
+        const { getByTestId } = render(
+            <Hero article={testArticle} />,
+            { wrapper: MemoryRouter }
+        );
+
+        expect(getByTestId('hero-section')).toHaveStyle(`background-color: #000`)
     });
 });
