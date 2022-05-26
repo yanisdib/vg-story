3d1b2de2cb698a94b7be4063aeef3040129da2ec
Yanis Dib <ynsprive@gmail.com>
2022-05-26 20:24:44 +0200

    Move styled components

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,109 +1,23 @@
 import { Link } from 'react-router-dom';
-import styled from 'styled-components';
 
 import { Article } from '../../interfaces';
 
+import {
+    Content,
+    FrontImage,
+    Preview,
+    Section,
+    Wrapper
+} from './Hero.styles';
+
 
 interface HeroProps {
-    article: Article;
+    readonly article: Article;
 }
-
-interface StyledSectionProps {
-    readonly background?: string;
-}
-
-
-const Section = styled.section<StyledSectionProps>`
-    background-color: ${({
-        background
-    }: StyledSectionProps) => background};
-    
-    @media (min-width: 375px) {
-        display: flex;
-        flex-direction: row;
-        height: 100vh;
-
-    }
-
-    @media (min-width: 950px) {
-        height: 95vh;
-    }
-`;
-
-const Wrapper = styled.div`
-    flex-direction: column;
-    width: 100%;
-    height: inherit;
-`;
-
-const Content = styled.div`
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    align-items: center;
-    width: inherit;
-    height: inherit;
-`;
-
-const Preview = styled.div`
-    display: flex;
-    flex-direction: column;
-    flex-basis: 50%;
-    justify-content: center;
-    align-items: center;
-    height: inherit;
-    padding: 0 calc(var(--ui-padding-y) - 25px);
-    
-    a {
-        color: #000;
-        text-decoration: none;
-
-        &:hover {
-            text-decoration: underline;
-        }
-    }
-
-    h1 {
-        margin-bottom: 22px;
-    }
-    
-    h4 {
-        align-self: flex-start;
-        margin: 10px 0;
-
-        span > a {
-            text-decoration: underline;
-
-            &:hover{
-                text-decoration: none;
-            }
-        }
-    }
-`;
-
-const FrontImage = styled.div`
-    display: flex;
-    flex-direction: column;
-    flex-basis: 50%;
-    justify-content: center;
-    align-items: center;
-    height: inherit;
-    background-color: #FFF;
-
-    a {
-        height: 100%;
-
-        img {
-            width: 100%;
-            height: 100%;
-            object-fit: cover;
-            object-position: center;
-        }
-    }
-`;
 
 
 function Hero({ article }: HeroProps) {
+    
     const {
         backgroundHex = '#000',
         frontImage,
@@ -122,8 +36,9 @@ function Hero({ article }: HeroProps) {
                     <Preview>
                         <h4>
                             {topics
-                                .map(topic => topic?.name)
-                                .join(' — ')}
+                                .map(topic => topic.name)
+                                .join(' — ')
+                            }
                         </h4>
                         <h1>
                             <Link to={`article/${permalink}`}>
