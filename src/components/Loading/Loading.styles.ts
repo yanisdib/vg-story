95a4ba8b7c914c94b6654a41d42a2ef8a1e96206
Yanis Dib <ynsprive@gmail.com>
2022-05-26 20:24:50 +0200

    Create file for styled components

---
+++ src/components/Loading/Loading.styles.ts
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+
+export const Component = styled.div`
+    display: flex;
+    flex-direction: row;
+    height: 100vh;
+    background-color: #ececec;
+`;
+
+export const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    height: 100%;
+`;
+
+export const Content = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+`;
