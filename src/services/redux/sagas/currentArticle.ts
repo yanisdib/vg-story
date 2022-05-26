d0b4ccdec78607b50a526fe6e3dce9e84c68bc36
Yanis Dib <ynsprive@gmail.com>
2022-05-27 00:01:51 +0200

    Create currentArticle saga

---
+++ src/services/redux/sagas/currentArticle.ts
@@ -0,0 +1,42 @@
+import { call, put, takeLatest } from '@redux-saga/core/effects';
+import { PayloadAction } from '@reduxjs/toolkit';
+
+import { Article } from '../../../interfaces';
+
+import { fetchArticleByPermalink } from '../../api/articles';
+
+import {
+    fetchCurrentArticleRequest,
+    fetchCurrentArticleSuccess
+} from '../currentArticle/currentArticleSlice';
+
+
+export function* fetchCurrentArticleSaga({ payload: permalink }: PayloadAction<string>) {
+    try {
+
+        const article: Article = yield call(fetchArticleByPermalink, permalink);
+
+        if (article) {
+
+            yield put(fetchCurrentArticleSuccess(article));
+        }
+        else {
+
+            const error: Error = new Error(`This article does not exist.`);
+
+            yield put({ type: 'articles/fetchCurrentArticleFailure', error });
+        }
+    } catch {
+
+        const error: Error = new Error(`Unsuccessful request. Try again.`);
+
+        yield put({ type: 'articles/fetchCurrentArticleFailure', error });
+    }
+}
+
+export default function* currentArticleSaga() {
+    yield takeLatest(
+        fetchCurrentArticleRequest,
+        fetchCurrentArticleSaga
+    );
+}
