acb3736fdda9956a9259abe765ea536ef6f2b255
Yanis Dib <ynsprive@gmail.com>
2022-05-26 20:23:37 +0200

    Create file for styled-components

---
+++ src/components/Footer/Footer.styles.ts
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+
+export const Component = styled.footer`
+    display: flex;
+    flex-direction: row;
+    height: 50vh;
+    background-color: #000;
+    color: #FFF;
+`;
+
+export const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    height: inherit;
+`;
+
+export const Content = styled.div`
+    display: flex;
+    flex-direction: row;
+    height: inherit;
+`;
