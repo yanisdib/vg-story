5deeac3e5a8c5c0d27c96f4ea9b23c6ebcc69703
Yanis Dib <ynsprive@gmail.com>
2022-05-26 20:25:05 +0200

    Move styled components

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -1,30 +1,9 @@
-import styled from 'styled-components';
-
 import { Spinner } from '../';
-
-
-const Component = styled.div`
-    display: flex;
-    flex-direction: row;
-    height: 100vh;
-    background-color: #ececec;
-`;
-
-const Wrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    width: 100%;
-    height: 100%;
-`;
-
-const Content = styled.div`
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    align-items: center;
-    width: 100%;
-    height: 100%;
-`;
+import {
+    Component,
+    Content,
+    Wrapper
+} from './Loading.styles';
 
 
 function Loading(): JSX.Element {
@@ -36,7 +15,7 @@ function Loading(): JSX.Element {
                 </Content>
             </Wrapper>
         </Component>
-    )
+    );
 }
 
 
