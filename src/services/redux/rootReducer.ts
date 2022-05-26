d0dc375c83798c6a638a9d4e37fbf69f7db9fff2
Yanis Dib <ynsprive@gmail.com>
2022-05-27 00:01:24 +0200

    Add currentArticle reducer

--- src/services/redux/rootReducer.ts
+++ src/services/redux/rootReducer.ts
@@ -1,10 +1,12 @@
 import { combineReducers } from '@reduxjs/toolkit';
 
 import articlesReducer from './articles/articlesSlice';
+import currentArticleReducer from './currentArticle/currentArticleSlice';
 
 
 const rootReducer = combineReducers({
     articles: articlesReducer,
+    currentArticle: currentArticleReducer,
 });
 
 export type AppState = ReturnType<typeof rootReducer>;
