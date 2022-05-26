44e385103a84d7e39d20fe983170ceedb933436a
Yanis Dib <ynsprive@gmail.com>
2022-05-26 23:59:22 +0200

    Add routes for specific articles

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,40 @@
-import { Route, Routes } from 'react-router-dom';
+import {
+  Navigate,
+  Route,
+  Routes
+} from 'react-router-dom';
 
-import { Footer } from './components';
+import { useCurrentArticle } from './hooks/useCurrentArticle';
+import withStatusHandler from './helpers/hoc/withStatusHandler';
+
+import { Article, Footer } from './components';
 import { Home } from './pages';
 
 
 function App(): JSX.Element {
+
+  const ArticleWrapper = () => {
+
+    const { currentArticle, error, status } = useCurrentArticle();
+    const ArticleWithStatusHandler = withStatusHandler(Article);
+
+    return (
+      <ArticleWithStatusHandler
+        error={error}
+        status={status}
+        {...{ currentArticle }}
+      />
+    );
+  }
+
   return (
     <main>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="article/*">
+          <Route index element={<Navigate to="/" replace />} />
+          <Route path=":permalink" element={<ArticleWrapper />} />
+        </Route>
       </Routes>
       <Footer />
     </main>
