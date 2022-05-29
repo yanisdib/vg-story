5b7a8d5b811807e5836ae321c93c2e419a1b04ca
Yanis Dib <ynsprive@gmail.com>
2022-05-29 18:18:37 +0200

    Create FeaturedArticlesGrid component

---
+++ src/components/FeaturedArticlesGrid/FeaturedArticlesGrid.tsx
@@ -0,0 +1,103 @@
+import { Link } from 'react-router-dom';
+
+import { Article } from '../../interfaces';
+
+import Container from '../Container/Container';
+
+import {
+    Card,
+    Column,
+    Content,
+    FullCard,
+    Grid,
+    Section,
+    ViewMore,
+    Wrapper
+} from './FeaturedArticlesGrid.styles';
+
+
+interface FeaturedArticlesGridProps {
+    featuredArticles: Article[];
+}
+
+
+function FeaturedArticlesGrid(
+    { featuredArticles }: FeaturedArticlesGridProps
+): JSX.Element {
+    return (
+        <Section>
+            <Wrapper>
+                <Container>
+                    <Content>
+                        <Grid>
+                            {featuredArticles
+                                .flatMap((article, index) => {
+
+                                    const {
+                                        body,
+                                        frontImage,
+                                        id,
+                                        permalink,
+                                        title,
+                                        topics
+                                    } = article;
+
+                                    return (index < 2) ? (
+                                        <Card key={id}>
+                                            <Link to={`article/${permalink}`}>
+                                                <img
+                                                    alt={frontImage?.alt ?? title}
+                                                    src={frontImage.src}
+                                                    title={frontImage?.title ?? title}
+                                                />
+                                            </Link>
+                                            <h5>
+                                                {topics
+                                                    .map(topic => topic.name)
+                                                    .join(' — ')}
+                                            </h5>
+                                            <Link to={`article/${permalink}`}>
+                                                <h3>{title}</h3>
+                                            </Link>
+                                            <p>{body}</p>
+                                        </Card>
+                                    ) : (
+                                        <FullCard key={id}>
+                                            <Column>
+                                                <h5>
+                                                    {topics
+                                                        .map(topic => topic.name)
+                                                        .join(' — ')}
+                                                </h5>
+                                                <Link to={`article/${permalink}`}>
+                                                    <h3>{title}</h3>
+                                                </Link>
+                                                <p>{body}</p>
+                                            </Column>
+                                            <Column>
+                                                <Link to={`article/${permalink}`}>
+                                                    <img
+                                                        alt={frontImage?.alt ?? title}
+                                                        src={frontImage.src}
+                                                        title={frontImage?.title ?? title}
+                                                    />
+                                                </Link>
+                                            </Column>
+                                        </FullCard>
+                                    );
+                                })}
+                        </Grid>
+                    </Content>
+                </Container>
+                <ViewMore>
+                    <Link to="/browse/articles">
+                        <h3>View all stories -&gt;</h3>
+                    </Link>
+                </ViewMore>
+            </Wrapper>
+        </Section>
+    );
+}
+
+
+export default FeaturedArticlesGrid;
