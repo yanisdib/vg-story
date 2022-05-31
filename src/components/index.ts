ce5fa1061ffe03ae278001ee813e23e2e8166b35
Yanis Dib <ynsprive@gmail.com>
2022-05-31 20:46:13 +0200

    Add new component imports

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,13 @@
 export { default as Container } from './Container/Container';
 export { default as Footer } from './Footer/Footer';
+export { default as Tag } from './Tag/Tag';
 
 export { default as Spinner } from './Spinner/Spinner';
 export { default as Loading } from './Loading/Loading';
+export { default as ImageUploader } from './ImageUploader/ImageUploader';
 
 export { default as Hero } from './Hero/Hero';
 export { default as FeaturedArticlesGrid } from './FeaturedArticlesGrid/FeaturedArticlesGrid';
 export { default as Article } from './Article/Article';
+
+export { default as CreateArticle } from './CreateArticle/CreateArticle';
