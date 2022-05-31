3e454df05d1d69d87c42a4b91f21e48f3b38f916
Yanis Dib <ynsprive@gmail.com>
2022-05-31 20:46:01 +0200

    Create ImageUploader component

---
+++ src/components/ImageUploader/ImageUploader.tsx
@@ -0,0 +1,42 @@
+import { ImageAttributes } from '../../interfaces';
+import { AddImageIcon, FileInput, PreviewCanvas, Uploader } from './ImageUploader.styles';
+
+
+interface ImageUploaderProps {
+    image?: ImageAttributes;
+    onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+
+function ImageUploader({ image, onChange }: ImageUploaderProps)
+    : JSX.Element {
+
+    return (
+        <>
+            {image && (
+                <PreviewCanvas
+                    alt={image.alt}
+                    data-testid="preview-canvas"
+                    src={image.src}
+                    title={image.title}
+                />
+            )}
+            <Uploader htmlFor="image-uploader">
+                <AddImageIcon />
+                Choose an image for your story
+                <small>
+                    You can only upload images of.jpg, .jpeg and.png format
+                </small>
+                <FileInput
+                    type="file"
+                    id="image-uploader"
+                    onChange={onChange}
+                    accept=".jpg, .jpeg, .png"
+                />
+            </Uploader>
+        </>
+    );
+}
+
+
+export default ImageUploader;
