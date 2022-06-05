47d845fa635e5e1be49d0b72d4c8c6964f38f85a
Yanis Dib <ynsprive@gmail.com>
2022-06-05 19:16:33 +0200

    Add topics saga and reducer

--- src/services/redux/rootReducer.ts
+++ src/services/redux/rootReducer.ts
@@ -1,12 +1,14 @@
 import { combineReducers } from '@reduxjs/toolkit';
 
-import articlesReducer from './articles/articlesSlice';
-import currentArticleReducer from './currentArticle/currentArticleSlice';
+import articlesReducer from './slices/articles/articlesSlice';
+import currentArticleReducer from './slices/currentArticle/currentArticleSlice';
+import topicsReducer from './slices/topics/topicsSlice';
 
 
 const rootReducer = combineReducers({
     articles: articlesReducer,
     currentArticle: currentArticleReducer,
+    topics: topicsReducer
 });
 
 export type AppState = ReturnType<typeof rootReducer>;
--- src/services/redux/rootSaga.ts
+++ src/services/redux/rootSaga.ts
@@ -2,11 +2,13 @@ import { all, fork } from '@redux-saga/core/effects';
 
 import articlesSaga from './sagas/articles';
 import currentArticleSaga from './sagas/currentArticle';
+import topicsSaga from './sagas/topics';
 
 
 export default function* rootSaga() {
     yield all([
         fork(articlesSaga),
-        fork(currentArticleSaga)
+        fork(currentArticleSaga),
+        fork(topicsSaga)
     ]);
 }
