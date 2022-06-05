44296451c868e333ba2444c7281328eabfc3ff49
Yanis Dib <ynsprive@gmail.com>
2022-06-05 19:11:31 +0200

    Create CreateArticle styles and component

---
+++ src/components/CreateArticle/CreateArticle.styles.ts
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+
+export const View = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+`;
+
+export const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    height: 100%;
+`;
+
+export const Content = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+`;
--- src/components/CreateArticle/CreateArticle.tsx
+++ src/components/CreateArticle/CreateArticle.tsx
@@ -1,5 +1,35 @@
+import { useDispatch } from 'react-redux';
+
+import { createArticleRequest } from '../../services/redux/slices/articles/articlesSlice';
+
+import { Article } from '../../interfaces';
+
+import { View, Content, Wrapper } from './CreateArticle.styles';
+import Container from '../Container/Container';
+import ArticleForm from '../ArticleForm/ArticleForm';
+import { addArticleToApi } from '../../services/api/articles';
+
+
 function CreateArticle(): JSX.Element {
-    return <div></div>;
+
+    const dispatch = useDispatch();
+
+    const handleFormSubmit = async (article: Article) => {
+        await addArticleToApi(article);
+        dispatch(createArticleRequest(article));
+    }
+
+    return (
+        <View>
+            <Wrapper>
+                <Container>
+                    <Content>
+                        <ArticleForm onSubmit={handleFormSubmit} />
+                    </Content>
+                </Container>
+            </Wrapper>
+        </View>
+    );
 }
 
 
